d93a5302a7c61fba1fb893436e33e178a4d1cab0
Maciej Milarski <maciekmilarski123@gmail.com>
2023-06-01 18:48:01 +0200

    added tests

--- tests/test_tmdb.py
+++ tests/test_tmdb.py
@@ -3,38 +3,30 @@ from unittest.mock import Mock
 
 
 def test_get_single_movie(monkeypatch):
-   # Przygotowanie danych testowych
    movie_id = 603692
    mock_response = {'id': movie_id, 'title': 'John Wick: Chapter 4'}
 
-   # Symulowanie żądania API za pomocą Mock() i monkeypatch
    mock_get = Mock(return_value=Mock(json=Mock(return_value=mock_response)))
    monkeypatch.setattr("requests.get", mock_get)
 
-   # Wywołanie funkcji testowanej
    movie = tmdb_client.get_single_movie(movie_id)
 
-   # Sprawdzenie wyników
    assert movie['id'] == movie_id
    assert movie['title'] == 'John Wick: Chapter 4'
 
 
 def test_get_movie_images(monkeypatch):
-   # Przygotowanie danych testowych
    movie_id = 603692
    mock_response = {
       'id': movie_id,
       'backdrops': [{'file_path': '/h8gHn0OzBoaefsYseUByqsmEDMY.jpg'}]
    }
 
-   # Symulowanie żądania API za pomocą Mock() i monkeypatch
    mock_get = Mock(return_value=Mock(json=Mock(return_value=mock_response)))
    monkeypatch.setattr("requests.get", mock_get)
 
-   # Wywołanie funkcji testowanej
    images = tmdb_client.get_movie_images(movie_id)
 
-   # Sprawdzenie wyników
    assert images['id'] == movie_id
    assert len(images['backdrops']) == 1
    assert images['backdrops'][0]['file_path'] == '/h8gHn0OzBoaefsYseUByqsmEDMY.jpg'
